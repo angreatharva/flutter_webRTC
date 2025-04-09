14a5d9970cd2a34c57c9efcf755d900b098adb76
angreatharva <angreatharva08@gmail.com>
2025-04-09 23:56:35 +0530

    video call perfect on single channel

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class VideoCallApp extends StatelessWidget {
   VideoCallApp({super.key});
 
   // WebSocket signaling server URL
-  final String websocketUrl = "https://4deb-103-104-226-58.ngrok-free.app";
+  final String websocketUrl = "https://5d8c-210-16-113-60.ngrok-free.app";
 
   // Randomly generate caller ID
   final String selfCallerID =
